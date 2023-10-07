7c01a226d251f4f89817172c29bc347b8deca34e
ngandt184163 <ngan.dt184163@sis.hust.edu.vn>
2023-10-07 17:10:12 +0700

    Initial commit

--- modules/home/views/homeView.php
+++ modules/home/views/homeView.php
@@ -7,13 +7,13 @@ get_header("", $data);
             <div class="section" id="slider-wp">
                 <div class="section-detail">
                     <div class="item">
-                        <img src="public/images/slider-01.png" alt="">
+                        <img src="/images/slider-01.png" alt="">
                     </div>
                     <div class="item">
-                        <img src="public/images/slider-02.png" alt="">
+                        <img src="/images/slider-02.png" alt="">
                     </div>
                     <div class="item">
-                        <img src="public/images/slider-03.png" alt="">
+                        <img src="/images/slider-03.png" alt="">
                     </div>
                 </div>
             </div>
@@ -22,35 +22,35 @@ get_header("", $data);
                     <ul class="list-item clearfix">
                         <li>
                             <div class="thumb">
-                                <img src="public/images/icon-1.png">
+                                <img src="/images/icon-1.png">
                             </div>
                             <h3 class="title">Miễn phí vận chuyển</h3>
                             <p class="desc">Tới tận tay khách hàng</p>
                         </li>
                         <li>
                             <div class="thumb">
-                                <img src="public/images/icon-2.png">
+                                <img src="/images/icon-2.png">
                             </div>
                             <h3 class="title">Tư vấn 24/7</h3>
                             <p class="desc">1900.9999</p>
                         </li>
                         <li>
                             <div class="thumb">
-                                <img src="public/images/icon-3.png">
+                                <img src="/images/icon-3.png">
                             </div>
                             <h3 class="title">Tiết kiệm hơn</h3>
                             <p class="desc">Với nhiều ưu đãi cực lớn</p>
                         </li>
                         <li>
                             <div class="thumb">
-                                <img src="public/images/icon-4.png">
+                                <img src="/images/icon-4.png">
                             </div>
                             <h3 class="title">Thanh toán nhanh</h3>
                             <p class="desc">Hỗ trợ nhiều hình thức</p>
                         </li>
                         <li>
                             <div class="thumb">
-                                <img src="public/images/icon-5.png">
+                                <img src="/images/icon-5.png">
                             </div>
                             <h3 class="title">Đặt hàng online</h3>
                             <p class="desc">Thao tác đơn giản</p>
@@ -66,7 +66,7 @@ get_header("", $data);
                     <ul class="list-item">
                         <li>
                             <a href="?mod=product&controller=index&action=detailProduct" title="" class="thumb">
-                                <img src="public/images/img-pro-05.png">
+                                <img src="/images/img-pro-05.png">
                             </a>
                             <a href="?mod=product&controller=index&action=detailProduct" title="" class="product-name">Laptop Lenovo IdeaPad 120S</a>
                             <div class="price">
@@ -80,7 +80,7 @@ get_header("", $data);
                         </li>
                         <li>
                             <a href="?mod=product&controller=index&action=detailProduct" title="" class="thumb">
-                                <img src="public/images/img-pro-08.png">
+                                <img src="/images/img-pro-08.png">
                             </a>
                             <a href="?page=detail_product" title="" class="product-name">Samsung Galaxy S8 Plus</a>
                             <div class="price">
@@ -94,7 +94,7 @@ get_header("", $data);
                         </li>
                         <li>
                             <a href="?page=detail_product" title="" class="thumb">
-                                <img src="public/images/img-pro-07.png">
+                                <img src="/images/img-pro-07.png">
                             </a>
                             <a href="?page=detail_product" title="" class="product-name">Laptop Acer Aspire ES1</a>
                             <div class="price">
@@ -108,7 +108,7 @@ get_header("", $data);
                         </li>
                         <li>
                             <a href="?page=detail_product" title="" class="thumb">
-                                <img src="public/images/img-pro-10.png">
+                                <img src="/images/img-pro-10.png">
                             </a>
                             <a href="?page=detail_product" title="" class="product-name">Sony Xperia XZ Premium</a>
                             <div class="price">
@@ -122,7 +122,7 @@ get_header("", $data);
                         </li>
                         <li>
                             <a href="?page=detail_product" title="" class="thumb">
-                                <img src="public/images/img-pro-06.png">
+                                <img src="/images/img-pro-06.png">
                             </a>
                             <a href="?page=detail_product" title="" class="product-name">Laptop Asus E402NA</a>
                             <div class="price">
@@ -136,7 +136,7 @@ get_header("", $data);
                         </li>
                         <li>
                             <a href="?page=detail_product" title="" class="thumb">
-                                <img src="public/images/img-pro-09.png">
+                                <img src="/images/img-pro-09.png">
                             </a>
                             <a href="?page=detail_product" title="" class="product-name">IPhone 7 128GB</a>
                             <div class="price">
@@ -164,7 +164,7 @@ get_header("", $data);
                                 ?>
                                 <li>
                                     <a href="?mod=product&controller=index&action=detailProduct&product_id=<?php echo $item['product_id']; ?>" title="" class="thumb">
-                                        <img src="public/images/<?php echo $item['image']; ?>">
+                                        <img src="/images/<?php echo $item['image']; ?>">
                                     </a>
                                     <a href="?mod=product&controller=index&action=detailProduct&product_id=<?php echo $item['product_id']; ?>" title="" class="product-name"><?php echo $item['name']; ?></a>
                                     <div class="price">
@@ -195,7 +195,7 @@ get_header("", $data);
                                 ?>
                                 <li>
                                     <a href="?mod=product&controller=index&action=detailProduct&product_id=<?php echo $item['product_id']; ?>" title="" class="thumb">
-                                        <img src="public/images/<?php echo $item['image']; ?>">
+                                        <img src="/images/<?php echo $item['image']; ?>">
                                     </a>
                                     <a href="?mod=product&controller=index&action=detailProduct&product_id=<?php echo $item['product_id']; ?>" title="" class="product-name"><?php echo $item['name']; ?></a>
                                     <div class="price">
