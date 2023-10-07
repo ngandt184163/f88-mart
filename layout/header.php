499c7724da3b561d4db1203d4a05682ec6f68902
ngandt184163 <ngan.dt184163@sis.hust.edu.vn>
2023-10-07 16:59:52 +0700

    Initial commit

--- layout/header.php
+++ layout/header.php
@@ -4,22 +4,22 @@
         <title>F88 STORE</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="public/css/bootstrap/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
-        <link href="public/css/bootstrap/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <link href="public/reset.css" rel="stylesheet" type="text/css"/>
-        <link href="public/css/carousel/owl.carousel.css" rel="stylesheet" type="text/css"/>
-        <link href="public/css/carousel/owl.theme.css" rel="stylesheet" type="text/css"/>
-        <link href="public/css/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-        <link href="public/style.css" rel="stylesheet" type="text/css"/>
-        <link href="public/responsive.css" rel="stylesheet" type="text/css"/>
+        <link href="/css/bootstrap/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
+        <link href="/css/bootstrap/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="/reset.css" rel="stylesheet" type="text/css"/>
+        <link href="/css/carousel/owl.carousel.css" rel="stylesheet" type="text/css"/>
+        <link href="/css/carousel/owl.theme.css" rel="stylesheet" type="text/css"/>
+        <link href="/css/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="/style.css" rel="stylesheet" type="text/css"/>
+        <link href="/responsive.css" rel="stylesheet" type="text/css"/>
 
-        <script src="public/js/jquery-2.2.4.min.js" type="text/javascript"></script>
+        <script src="/js/jquery-2.2.4.min.js" type="text/javascript"></script>
         
         <!-- <script src="public/js/code.jquery.com_jquery-3.7.1.min.js" type="text/javascript"></script> -->
-        <script src="public/js/elevatezoom-master/jquery.elevatezoom.js" type="text/javascript"></script>
-        <script src="public/js/bootstrap/bootstrap.min.js" type="text/javascript"></script>
-        <script src="public/js/carousel/owl.carousel.js" type="text/javascript"></script>
-        <script src="public/js/main.js" type="text/javascript"></script>
+        <script src="/js/elevatezoom-master/jquery.elevatezoom.js" type="text/javascript"></script>
+        <script src="/js/bootstrap/bootstrap.min.js" type="text/javascript"></script>
+        <script src="/js/carousel/owl.carousel.js" type="text/javascript"></script>
+        <script src="/js/main.js" type="text/javascript"></script>
 
     </head>
     <body>
